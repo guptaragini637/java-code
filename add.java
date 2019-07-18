8ca3f3b64e9d474a1440eba1dec406d668816e72
guptaragini637 <53034048+guptaragini637@users.noreply.github.com>
2019-07-18 12:29:38 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Add
+{
+public static void main(String args[])
+{
+int x, y, z;
+System. out. println("Enter two number");
+Scanner in = new Scanner(System. in);
+x=nextInt();
+y=nextInt();
+z=x+y;
+System.out.prinln("Addition is"+z);
+}
+}
